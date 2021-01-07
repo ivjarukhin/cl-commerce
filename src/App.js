b5c67273f5c09b15b1b629a0f4c6d03ce4f385a2
Stefania-26 <stefanijaaa26@gmail.com>
2021-01-07 23:40:56 +0200

    init sign-in

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Header, {  } from "./components/header/header.components";
+import Header from "./components/header/header.components";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </switch>
     </div>
 

77b9d0a8c7fc0358c8ac3901c0512dbf0c9064b0
Stefania-26 <stefanijaaa26@gmail.com>
2021-01-09 13:23:17 +0200

    save user to firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,34 @@ const config = {
     measurementId: "G-XWL6J75R4P"
 }
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exist) {
+        const {displayName, email} = userAuth;
+        const createAt = new Date();
+
+        try {
+            userRef.set({
+                displayName,
+                email,
+                createAt,
+                ...additionalData
+            })
+
+        }
+        catch (error) {
+            console.log('error create', error.message)
+        }
+    }
+
+    return userRef
+}
+
 firebase.initializeApp(config);
 
 export const auth=firebase.auth();

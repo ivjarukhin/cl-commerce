10a64a74caa602aaca50b14a197bbf212b4380cf
Stefania-26 <stefanijaaa26@gmail.com>
2021-01-23 17:04:23 +0200

    checkout item

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,9 +7,10 @@ import "./cart-dropdown.styles.scss";
 
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 
-const CartDropdown = ({cartItems, history}) => (
+const CartDropdown = ({cartItems, history, dispatch}) => (
     <div className="cart-dropdown">
         <div className="cart-items">
             {cartItems.length ?(
@@ -19,7 +20,10 @@ const CartDropdown = ({cartItems, history}) => (
                <span className='empty-message'> Your cart is empty</span>        
         )}
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+             history.push('/checkout');
+             dispatch(toggleCartHidden); }
+        }> GO TO CHECKOUT</CustomButton>
     
     </div>
 );

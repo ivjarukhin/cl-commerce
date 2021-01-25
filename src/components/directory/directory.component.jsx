d1e3117a0b95944044467aa64ca1d306979cf2e4
Stefania-26 <stefanijaaa26@gmail.com>
2021-01-26 00:32:11 +0200

    overview component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,61 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import { MenuItem } from '../menu-item/menu-item.component'
+import {  selectDirectorySections } from "../../redux/directory/directory.selectors";
 
 import "./directory.styles.scss";
 
-class Directory extends React.Component {
-    constructor() {
-        super();
+const Directory = ({ sections }) => (
+    <div className="directory-menu">
+      {
+          sections.map(({ id, ...sectionProps }) =>
+              <MenuItem kye={id} {...sectionProps}/>)
+      }
+    </div>
+)
 
-        this.state = {
-            sections: [
-                {
-                  title: 'hats',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1,
-                  linkUrl: 'shop/hats'
-                },
-                {
-                  title: 'jackets',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2,
-                  linkUrl: 'shop/jackets'
-                },
-                {
-                  title: 'sneakers',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3,
-                  linkUrl: 'shop/sneakers'
-                },
-                {
-                  title: 'womens',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                  id: 4,
-                  linkUrl: 'shop/womens',
-                  size: 'large'
-                },
-                {
-                  title: 'mens',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                  id: 5,
-                  linkUrl: 'shop/mens',
-                  size: 'large'
-                }
-              ]
-        }
-    }
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+})
 
-    render() {
-        return (
-            <div className="directory-menu">
-            {
-                this.state.sections.map(({ id, ...sectionProps }) =>
-                   <MenuItem kye={id} {...sectionProps}/>)
-            }
-            </div>
-        )
-    }
-}
-
-export default Directory;
+export default connect(mapStateToProps)(Directory);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,10 @@
 import React from "react";
 
-import SHOP_DATA from './shop.data.js'
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-
-class ShopPage extends React.Component {
-    constructor (props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const {collections} = this.state;
-        return (<div className="shop-page">
-        {
-            collections.map(({id, ...otherCollectionsProps}) => (
-                <CollectionPreview key={id} {...otherCollectionsProps} />
-            ))
-        }
-        </div>)
-    }
-}
+import CollectionsOverview from "../../components/collections-overview/collections-overview.components";
+const ShopPage = ({ collections }) => (
+    <div className="shop-page">
+        <CollectionsOverview />
+    </div>
+);
 
 export default ShopPage;

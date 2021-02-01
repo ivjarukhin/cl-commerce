46ac592b3ac83b9952bea868403cbabdcd06a548
Stefania-26 <stefanijaaa26@gmail.com>
2021-02-01 22:54:46 +0200

    promise completing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -10,6 +10,7 @@ import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/fireb
 import { updateCollections } from "../../redux/shop/shop.actions";
 
 import WithSpinner from "../../components/with-spinner/with-spinner.component";
+import { withTheme } from "styled-components";
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
@@ -36,10 +37,11 @@ class ShopPage extends React.Component {
     }
     render() {
         const { match } = this.props;
+        const { loading } = this.state;
         return (
             <div className="shop-page">
-                <Route exact path={`${match.path}`} component={CollectionsOverview} />
-                <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+                <Route exact path={`${match.path}`} render={props => (<CollectionsOverviewWithSpinner isLoading={loading} {...props} />)} />
+                <Route path={`${match.path}/:collectionId`} render={props => (<CollectionPageWithSpinner isLoading={loading} {...props} />)} />
             </div>
         )
     }

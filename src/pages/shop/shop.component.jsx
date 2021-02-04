d1701e10a5e5cbd8888a3d131e3945fc8d6786a5
Stefania-26 <stefanijaaa26@gmail.com>
2021-02-05 00:03:40 +0200

    setup thunx

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,59 +1,52 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { createStructuredSelector } from "reselect";
 import { connect } from "react-redux";
 
 import CollectionPage from "../collection/collection.component";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.components";
 
-import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils";
-import { updateCollections } from "../../redux/shop/shop.actions";
+import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
+import { selectIsCollectionFetching, selectIsCollectionsLoaded } from "../../redux/shop/shop.selectors";
 
 import WithSpinner from "../../components/with-spinner/with-spinner.component";
-import { withTheme } from "styled-components";
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
-    state = {
-        loading: true
-    }
-
-    unsubsribeFromSnapshot = null;
 
     componentDidMount() {
-        const { updateCollections } = this.props;
-        const collectionRef = firestore.collection('collections');
+        const { fetchCollectionsStartAsync } = this.props;
+        fetchCollectionsStartAsync();
 
-        this.unsubsribeFromSnapshot = collectionRef.onSnapshot( async snapShot =>
-        collectionRef.get().then(snapshot =>
-            {   
-                const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-                updateCollections(collectionsMap);
-                this.setState({ loading: false });
-            }
-        ))
+
+        //this.unsubsribeFromSnapshot = collectionRef.onSnapshot( async snapShot =>
+
 
         // fetch('https://firestore.googleapis.com/v1/projects/compr_db/databases/(default)/documents/collections')
         // .then(response => response.json())
 
     }
     render() {
-        const { match } = this.props;
-        const { loading } = this.state;
+        const { match, isCollectionFetching, isCollectionsLoaded } = this.props;
         return (
             <div className="shop-page">
-                <Route exact path={`${match.path}`} render={props => (<CollectionsOverviewWithSpinner isLoading={loading} {...props} />)} />
-                <Route path={`${match.path}/:collectionId`} render={props => (<CollectionPageWithSpinner isLoading={loading} {...props} />)} />
+                <Route exact path={`${match.path}`} render={props => (<CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props} />)} />
+                <Route path={`${match.path}/:collectionId`} render={props => (<CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props} />)} />
             </div>
         )
     }
 }
 
 const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap => 
-        dispatch(updateCollections(collectionsMap))
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 });
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+const mapStateToProps = createStructuredSelector({
+    isCollectionFetching: selectIsCollectionFetching,
+    isCollectionsLoaded: selectIsCollectionsLoaded
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -3,15 +3,29 @@
 import ShopActionTypes from "./shop.types";
 
 const INITIAL_STATE = {
-    collections: null
+    collections: null,
+    isFetching: false,
+    errorMessage: undefined
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case ShopActionTypes.UPDATE_COLLECTIONS:
+        case ShopActionTypes.FETCH_COLLECTIONS_START:
             return {
                 ...state,
+                isFatching: true
+            }
+        case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
+            return {
+                ...state,
+                isFatching: false,
                 collections: action.payload
+            }
+        case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
+            return {
+                ...state,
+                isFatching: false,
+                errorMessage: action.payload
             }
         default:
             return state;
@@ -19,3 +33,23 @@ const shopReducer = (state = INITIAL_STATE, action) => {
 };
 
 export default shopReducer;
+
+// import ShopActionTypes from "./shop.types";
+
+// const INITIAL_STATE = {
+//     collections: null
+// };
+
+// const shopReducer = (state = INITIAL_STATE, action) => {
+//     switch (action.type) {
+//         case ShopActionTypes.UPDATE_COLLECTIONS:
+//             return {
+//                 ...state,
+//                 collections: action.payload
+//             }
+//         default:
+//             return state;
+//     }
+// };
+
+// export default shopReducer;

749b413bafa9d6168c370eb6dfcac5232fe1b1d5
IvYar <jarukhin.io@gmail.com>
2021-02-13 14:59:46 +0200

    init and root saga

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,13 +7,13 @@ import CollectionPageContainer from "../collection/collection.container";
 
 import CollectionsOverviewContainer from "../../components/collections-overview/collection-overview.container";
 
-import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
 class ShopPage extends React.Component {
 
     componentDidMount() {
-        const { fetchCollectionsStartAsync } = this.props;
-        fetchCollectionsStartAsync();
+        const { fetchCollectionsStart } = this.props;
+        fetchCollectionsStart();
 
 
         //this.unsubsribeFromSnapshot = collectionRef.onSnapshot( async snapShot =>
@@ -37,7 +37,7 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
